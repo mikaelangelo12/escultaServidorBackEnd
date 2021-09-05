fc4ac1086afd36d89be7fb6507cba2fc5d2ec877
mikaelangelo12 <mikaelangelo12@gmail.cm>
2021-09-05 00:38:56 -0300

    Criando exibeMenu

--- meuBackEnd.go
+++ meuBackEnd.go
@@ -2,5 +2,11 @@ package main
 import "fmt"
 
 func main(){
-	
+	exibeMenu()
+}
+func exibeMenu(){
+	fmt.Println("1 - Esculta Servidor Local")
+	fmt.Println("2 - Verificar logs")
+	fmt.Println("3 - Servidor Internet")
+	fmt.Println("0 - Sair")
 }
